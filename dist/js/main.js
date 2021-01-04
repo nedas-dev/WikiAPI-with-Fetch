2cce3e7648d4b81e4b7822218483c5574da9b758
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-01-04 12:44:24 -0800

    local storage left to update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,93 +1,109 @@
-const astronautsUrlAPI = 'json/data.json';
 const wikiUrlAPI = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
 const mainSection = document.querySelector('section.people');
-const submitButton = document.querySelector('button')
+const addPersonForm = document.querySelector('#addPerson');
+const formsButton = document.querySelector('button.btn');
+const formsInput = document.querySelector('input.form-control');
+const alertDiv = document.querySelector('div.alert');
 
-/* This function gets and prepares JSON file ready to use from any online API */
-function getJSON(url) {
-    return new Promise((resolve, reject) => {
-        const xml = new XMLHttpRequest();
-        xml.onload = () => {
-            if (xml.status == 200) {
-                resolve(JSON.parse(xml.responseText));
-            } else {
-                reject(Error('Could not get the JSON file'));
-            }
-
-        }
-        xml.onerror = () => {
-            reject(Error('Network error. Did you type in the correct link?'));
-        }
-        xml.open('GET', url);
-        xml.send();
-    })
+// function to check if we received data successfully
+function personExists(data) {
+    if (!data.ok) {
+        successAndErrorAlert('failed');
+        throw new Error('Could not attain the person.')
+    } else {
+        successAndErrorAlert('success');
+    }
+    return data
 }
 
-// Gets json files from wiki API about cosmonauts that are in data variable
-function getProfiles(data) {
-    const promisesList = data.people.map(people => {
-        return getJSON(wikiUrlAPI + people.name)
-    })
-    return Promise.all(promisesList);
+// alert pop-up when you un(successfully) add a search result
+function successAndErrorAlert(string) {
+    if (string == 'success') {
+        alertDiv.style.display = 'block';
+        alertDiv.style.opacity = 1;
+        alertDiv.className = "alert alert-success text-center";
+        alertDiv.innerHTML = `You have successfully added <strong>${formsInput.value}!<i class="fas fa-times clickme"></i></strong>`
+    } else if (string == 'failed') {
+        alertDiv.style.display = 'block';
+        alertDiv.style.opacity = 1;
+        alertDiv.className = "alert alert-danger text-center";
+        alertDiv.innerHTML = `<strong>${formsInput.value}</strong> was not found. Try again <i class="fas fa-times clickme"></i>`
+    } else if (string == 'x') {
+        alertDiv.style.opacity = 0;
+        setTimeout(() => {
+            alertDiv.style.display = 'none';
+        }, 300)
+    }
 }
 
-// This func prepares each profile (div) with information about each astronaut and appends it the the DOM
-function modifyAndAppendDivToDOM(div, person, pictureExists) {
+// call fetch API when we submit data (specific known person, country, religious and etc...)
+addPersonForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(wikiUrlAPI + formsInput.value)
+        .then(person => personExists(person))
+        .then(person => person.json())
+        .then(generateHTML)
+        .catch(err => console.log(err))
+        .finally(() => formsInput.value = '')
+})
+
+
+
+// Function to create a profile for a search result and update it in the DOM.
+// Why there are two different methods for profiles?
+// The answer is - one for search result which contains an image and the other not.
+function modifyAndAppendProfileToDOM(div, person, pictureExists) {
     if (pictureExists) {
         div.innerHTML = `
         <img src=${person.thumbnail.source}>
-        <div>
+        <div class="description">
             <h2>${person.displaytitle}</h2>
             <p class="important"><strong>${person.description}</strong></p>
             <p class="regular">${person.extract}</p>
         </div>
+        <i class="fas fa-trash-alt trash"></i>
         `
-        const profilesExist = mainSection.children.length > 0;
-        if (profilesExist) {
-            const firstChild = mainSection.firstElementChild;
-            mainSection.insertBefore(div, firstChild)
-        } else {
-            mainSection.appendChild(div);
-        }
     } else {
         div.innerHTML = `
-        <img width="260" height="340" src="https://i.pinimg.com/736x/96/34/62/9634629ee6707f329d6194ceded5f0fe.jpg">
-        <div>
+        <img width="280" height="340" src="https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg">
+        <div class="description">
             <h2>${person.displaytitle}</h2>
             <p class="important>${person.description}</p>
             <p class="regular">${person.extract}</p>
         </div>
+        <i class="fas fa-trash-alt trash"></i>
         `
+    }
+    // Every new added search will be added to the top of the list
+    // instead of going down the list
+    const profilesExist = mainSection.children.length > 0;
+    if (profilesExist) {
+        const firstChild = mainSection.firstElementChild;
+        mainSection.insertBefore(div, firstChild)
+    } else {
         mainSection.appendChild(div);
     }
 }
 
-// from getProfiles here data gives us a list of wiki API summary about each cosmonaut so we want to extract json files, generate and add - HTML for our "website"
-function generateHTML(data) {
-    data.forEach(person => {
-        const div = document.createElement('div');
-        div.className = "profile";
-        const pictureExists = 'thumbnail' in person;
-        modifyAndAppendDivToDOM(div, person, pictureExists);
-    });
+// this function creates profile div and passes it to the function  modifyAndAppendProfileToDOM which will
+// take care of creating content for profile div and updating it to the DOM.
+function generateHTML(person) {
+    const div = document.createElement('div');
+    div.className = "profile";
+    const pictureExists = 'thumbnail' in person;
+    modifyAndAppendProfileToDOM(div, person, pictureExists);
 }
 
+// event listener to make disappear ALERT MESSAGE (green/red alert message up the page)
+alertDiv.addEventListener('click', (e) => {
+    if (e.target.tagName == 'I') {
+        successAndErrorAlert('x');
+    }
+})
 
-// Each function will be completed in sequence (That is how Promises work because we are trying to get an asynchronous data)
-// .then() will run each time it will be successfully completed (Resolved)
-// .catch() will run if it will run into reject() function which means something failed in the Promise.
-// .finally() will only run if all promises were resolved successfully (all ran into resolve())
-const activateRequest = (event) => {
-    getJSON(astronautsUrlAPI)
-        .then(getProfiles)
-        .then(generateHTML)
-        .catch(error => {
-            mainSection.innerHTML = "<h3 style='text-align: center; padding: 20px; background-color:white;'>Something went wrong...</h3>"
-        })
-        .finally(() => event.target.remove())
-}
-
-submitButton.addEventListener('click', (e) => {
-    e.target.textContent = "Loading...";
-    activateRequest(e);
+// event listener for deleting a profile of search result (trash can icon)
+mainSection.addEventListener('click', (e) => {
+    if (e.target.tagName == 'I') {
+        e.target.parentNode.remove();
+    }
 })
