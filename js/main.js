9a54188ff6b983339082b6c90dba3ced8fcbc3f7
Nedas Snarskis <nedas.snarskis17@gmail.com>
2021-01-04 15:39:02 -0800

    Updates

--- js/main.js
+++ js/main.js
@@ -5,49 +5,73 @@ const formsButton = document.querySelector('button.btn');
 const formsInput = document.querySelector('input.form-control');
 const alertDiv = document.querySelector('div.alert');
 
+// Alert messages
+const SUCCESS = 'SUCCESS';
+const FAILED = 'FAILED';
+const X = 'X';
+
 // function to check if we received data successfully
-function personExists(data) {
+function searchExists(data) {
     if (!data.ok) {
-        successAndErrorAlert('failed');
+        if (formsInput.value) {
+            successAndErrorAlert(FAILED);
+        }
         throw new Error('Could not attain the person.')
-    } else {
-        successAndErrorAlert('success');
     }
     return data
 }
 
 // alert pop-up when you un(successfully) add a search result
-function successAndErrorAlert(string) {
-    if (string == 'success') {
-        alertDiv.style.display = 'block';
-        alertDiv.style.opacity = 1;
-        alertDiv.className = "alert alert-success text-center";
-        alertDiv.innerHTML = `You have successfully added <strong>${formsInput.value}!<i class="fas fa-times clickme"></i></strong>`
-    } else if (string == 'failed') {
-        alertDiv.style.display = 'block';
-        alertDiv.style.opacity = 1;
-        alertDiv.className = "alert alert-danger text-center";
-        alertDiv.innerHTML = `<strong>${formsInput.value}</strong> was not found. Try again <i class="fas fa-times clickme"></i>`
-    } else if (string == 'x') {
-        alertDiv.style.opacity = 0;
-        setTimeout(() => {
-            alertDiv.style.display = 'none';
-        }, 300)
+function successAndErrorAlert(message) {
+    const alert = {
+        SUCCESS: () => {
+            alertDiv.style.display = 'block';
+            alertDiv.style.opacity = 1;
+            alertDiv.className = "alert alert-success text-center";
+            alertDiv.innerHTML = `You have successfully added <strong>${formsInput.value}!<i class="fas fa-times clickme"></i></strong>`
+        },
+        FAILED: () => {
+            alertDiv.style.display = 'block';
+            alertDiv.style.opacity = 1;
+            alertDiv.className = "alert alert-danger text-center";
+            alertDiv.innerHTML = `<strong>${formsInput.value}</strong> was not found. Try again <i class="fas fa-times clickme"></i>`
+        },
+        X: () => {
+            alertDiv.style.opacity = 0;
+            setTimeout(() => {
+                alertDiv.style.display = 'none';
+            }, 300)
+        }
     }
+    alert[message]()
 }
 
-// call fetch API when we submit data (specific known person, country, religious and etc...)
-addPersonForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    fetch(wikiUrlAPI + formsInput.value)
-        .then(person => personExists(person))
+
+// API to fetch from WikiAPI and append it to the page.
+function fetchWikiAPI(string) {
+    fetch(wikiUrlAPI + string)
+        .then(person => searchExists(person))
         .then(person => person.json())
+        .then(person => {
+            if (formsInput.value) {
+                successAndErrorAlert(SUCCESS);
+                modifyLocalStorage(person['displaytitle']);
+            }
+            return person;
+        })
         .then(generateHTML)
         .catch(err => console.log(err))
         .finally(() => formsInput.value = '')
-})
+}
 
-
+// this function creates profile div and passes it to the function  modifyAndAppendProfileToDOM which will
+// take care of creating content for profile div and updating it to the DOM.
+function generateHTML(person) {
+    const div = document.createElement('div');
+    div.className = "profile";
+    const pictureExists = 'thumbnail' in person;
+    modifyAndAppendProfileToDOM(div, person, pictureExists);
+}
 
 // Function to create a profile for a search result and update it in the DOM.
 // Why there are two different methods for profiles?
@@ -85,25 +109,82 @@ function modifyAndAppendProfileToDOM(div, person, pictureExists) {
     }
 }
 
-// this function creates profile div and passes it to the function  modifyAndAppendProfileToDOM which will
-// take care of creating content for profile div and updating it to the DOM.
-function generateHTML(person) {
-    const div = document.createElement('div');
-    div.className = "profile";
-    const pictureExists = 'thumbnail' in person;
-    modifyAndAppendProfileToDOM(div, person, pictureExists);
+
+// LOCAL STORAGE ------------------------------------------------------------
+// Save all the search results for next time you will comeback.
+
+// Checks if user's browser supports localStorage
+function supportsLocalStorage() {
+    return 'localStorage' in window && window['localStorage'] !== null;
 }
 
-// event listener to make disappear ALERT MESSAGE (green/red alert message up the page)
-alertDiv.addEventListener('click', (e) => {
+// This function checks if browser supports local storage and if
+// it does support appends the search result into the storage.
+function modifyLocalStorage(fulltitle) {
+    if (supportsLocalStorage()) {
+        if (localStorage['profiles']) {
+            checkAndAppendNewSearch(fulltitle);
+        } else {
+            localStorage.setItem('profiles', `["${fulltitle}"]`);
+        }
+    }
+}
+
+// This function checks if search result already exists in local storage else appends the new result.
+function checkAndAppendNewSearch(inputValue) {
+    const profileList = JSON.parse(localStorage.getItem('profiles'));
+    if (profileList.indexOf(inputValue) == -1) {
+        profileList.unshift(inputValue);
+        localStorage.setItem('profiles', JSON.stringify(profileList));
+    }
+}
+
+
+// Function to load all the profiles that were active before the page was closed
+function loadProfiles() {
+    if (supportsLocalStorage()) {
+        if (localStorage['profiles']) {
+            const profilesList = JSON.parse(localStorage.getItem('profiles'));
+            profilesList.forEach(profile => {
+                fetchWikiAPI(profile);
+            })
+        }
+    }
+}
+
+// Event listener for deleting search result (profile) when pressing trash can icon
+mainSection.addEventListener('click', (e) => {
     if (e.target.tagName == 'I') {
-        successAndErrorAlert('x');
+        const displaytitle = e.target.previousElementSibling.querySelector('h2').textContent;
+        if (supportsLocalStorage()) {
+            const profileList = JSON.parse(localStorage.getItem('profiles'))
+            index = profileList.indexOf(displaytitle);
+            if (index > -1) {
+                profileList.splice(index, 1)
+                localStorage.setItem('profiles', JSON.stringify(profileList));
+            }
+        }
+        e.target.parentNode.remove();
     }
 })
 
-// event listener for deleting a profile of search result (trash can icon)
-mainSection.addEventListener('click', (e) => {
+
+// call fetch API when we submit data (specific known person, country, religious and etc...)
+addPersonForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formsInput.value) {
+        fetchWikiAPI(formsInput.value)
+    }
+})
+
+// Once the page loads - we also want to load the profiles that were opened before the person closed the browser (or refreshed the page)
+window.onload = () => {
+    loadProfiles();
+}
+
+// event listener to remove ALERT MESSAGE (green/red alert message up the page)
+alertDiv.addEventListener('click', (e) => {
     if (e.target.tagName == 'I') {
-        e.target.parentNode.remove();
+        successAndErrorAlert(X);
     }
 })
